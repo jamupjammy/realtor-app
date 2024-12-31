3a7567618e6febd215692683fbf9cc3554127aa9
nikhil <nikhilgoyal007@gmail.com>
2024-12-30 22:38:35 -0500

    added image (from another table) to the home response using 'select' in prisma.

--- src/home/dto/home.dto.ts
+++ src/home/dto/home.dto.ts
@@ -28,6 +28,8 @@ export class HomeResponseDto {
   }
   price: number;
 
+  image: string;
+
   @Exclude()
   land_size: number;
   @Expose({ name: 'landSize' })
--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -6,7 +6,26 @@ import { HomeResponseDto } from './dto/home.dto';
 export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
   async getHomes(): Promise<HomeResponseDto[]> {
-    const homes = await this.prismaService.home.findMany();
-    return homes.map((home) => new HomeResponseDto(home));
+    const homes = await this.prismaService.home.findMany({
+      select: {
+        id: true,
+        address: true,
+        city: true,
+        price: true,
+        number_of_bedrooms: true,
+        number_of_bathrooms: true,
+        images: {
+          select: {
+            url: true,
+          },
+          take: 1,
+        },
+      },
+    });
+    return homes.map((home) => {
+      const fetchedHome = { ...home, image: home.images[0].url };
+      delete fetchedHome.images;
+      return new HomeResponseDto(fetchedHome);
+    });
   }
 }
